ba1611fc8957ee5843d14edb256400fc34310abe
oimou <s1190013@gmail.com>
2014-05-31 00:08:50 +0900

    Add watch task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var jade = require('gulp-jade');
 
-gulp.task('jade', function() {
+gulp.task('jade', function () {
   var YOUR_LOCALS = {};
 
   gulp.src('./public/*.jade')
     .pipe(jade())
     .pipe(gulp.dest('./public/'))
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./public/*.jade', ['jade']);
 });
 
 gulp.task('default', ['jade']);

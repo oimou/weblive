dcabe16b8b405afbce0d74682df5a86996324abe
oimou <s1190013@gmail.com>
2014-05-31 02:03:40 +0900

    Add AudioModel

---
+++ public/model/AudioModel.js
@@ -0,0 +1,25 @@
+define(function (require) {
+  'use strict';
+
+  var BufferLoader = require('lib/buffer-loader/buffer-loader');
+
+  return Backbone.Model.extend({
+    initialize: function (attr) {
+      var audio = this.audio = new Audio();
+      audio.addEventListener('canplaythrough', _(this.onLoad).bind(this));
+      audio.src = attr.src;
+
+      this.ctx = new AudioContext();
+    },
+
+    start: function () {
+      var src = this.ctx.createMediaElementSource(this.audio);
+      src.connect(this.ctx.destination);
+      this.audio.play();
+    },
+
+    onLoad: function () {
+      this.trigger('load');
+    }
+  });
+});
